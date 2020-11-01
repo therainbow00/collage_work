eadca26786f5ad422efc8a72905a2dcc149e0e7a
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2020-11-01 01:06:01 -0400

    changes

--- cs202/h9/d_hex.c
+++ cs202/h9/d_hex.c
@@ -1,16 +1,16 @@
 /*
   Read from the console.  Sample transcript...
 
-cs202@cs:~/solutions/h9$ ./d_hex.o 
+cs202@cs:~/solutions/h9$ ./d_hex.o
 Enter in he(x) or unsigned (d)ecimal? x
 Hex int: ff
 hex: ff, decimal: 255
 
-cs202@cs:~/solutions/h9$ ./d_hex.o 
+cs202@cs:~/solutions/h9$ ./d_hex.o
 Enter in he(x) or unsigned (d)ecimal? 255
 Invalid, expected x or d.  Goodbye.
 
-cs202@cs:~/solutions/h9$ ./d_hex.o 
+cs202@cs:~/solutions/h9$ ./d_hex.o
 Enter in he(x) or unsigned (d)ecimal? d
 Decimal int: 255
 hex: ff, decimal: 255
@@ -24,6 +24,24 @@ hex: ff, decimal: 255
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
-  
+
+  printf("Enter in he(x) or unsigned (d)ecimal? ");
+  char string[1];
+  scanf("%s", string);
+  unsigned char hex = 0xff;
+
+  if (string[0] == 'x')
+  {
+      printf("Hex int: %x\n", hex);
+      printf("hex: %x, decimal: %d\n", hex, hex);
+      return 0;
+  }
+  else if (string[0] == 'd')
+  {
+      printf("Decimal int: %d\n", hex);
+      printf("hex: %x, decimal: %d\n", hex, hex);
+      return 0;
+  }
+  printf("Invalid, expected x or d.  Goodbye.\n");
   return 0;
 }
