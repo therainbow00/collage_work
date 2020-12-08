ceaf38acf5579b72c66def17d355526ad65130fb
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2020-12-07 21:55:22 -0600

    changes

--- cs202/h13/p_struct_data.c
+++ cs202/h13/p_struct_data.c
@@ -6,10 +6,51 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+struct info
+{
+    int year;
+    int day;
+    char month[51];
+    float high_tem;
+};
+
+void get_data(struct info *data)
+{
+    struct info * d = (struct info *) malloc(sizeof(struct info));
+
+    printf("Year: ");
+    scanf("%d", &d->year);
+
+    printf("Month: ");
+    scanf("%50s", d->month);
+
+    printf("Day: ");
+    scanf("%d", &d->day);
+
+    printf("Temperature (a floating point #): ");
+    scanf("%f", &d->high_tem);
+}
+void print_data(struct info *data)
+{
+    if (data == NULL)
+    {
+        printf("Perameter has nothing\n");
+    }
+    else
+    {
+        printf("%lf degrees on %s/%d/%d\n", data->high_tem, data->month, data->day, data->year);
+    }
+}
 
 int main(int argc, char *argv[]) {
-
-
+  struct info * data = NULL;
+  if (data == NULL)
+  {
+      get_data(data);
+      print_data(data);
+  }
 
   return 0;
 }
