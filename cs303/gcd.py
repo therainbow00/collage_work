4526fd45b97a00bb037aa30cbe6ee34807800846
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-01-21 10:52:42 -0500

    changes

--- cs303/gcd.py
+++ cs303/gcd.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 
 # a >= 0, b > 0
+# factors of a0
 
 a0 = 6475566067978806192067593014888500403734149837672028290697869
 b0 = 2248165830212856236971484909139735123
@@ -8,12 +10,15 @@ a = a0
 b = b0
 r = a % b
 steps = 0
+# GCD part
 while r > 0:
     a = b
     b = r
     r = a % b
     steps += 1
 print(b)
-print(a0 % b)
-print(b0 % b)
-print("num of steps = ", steps )
+# end of GCD
+#print(a0 % b)
+#print(b0 % b)
+#print("num of steps = ", steps)
+frac = b0 / a0
