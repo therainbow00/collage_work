58e8308631d14eb80534458dd47dd3cd5cf0fe29
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-02-28 22:03:52 -0500

    changes

---
+++ cs101/labs/lab2/sum.py
@@ -0,0 +1,19 @@
+# Collect first number
+print("Enter a number:")
+user_data_1 = input()
+num1 = int(user_data_1)
+
+# Collect second number
+# YOU ADD CODE
+print("Enter a number:")
+user_data_2 = input()
+num2 = int(user_data_2)
+
+
+# ...
+# Collected user input numbers, twice, above.
+# ...
+
+result = num1 + num2
+
+print("The sum is " +  str(result) + "!")
