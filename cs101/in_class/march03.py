19e0c7c366df60199f775a024752d21c2b846465
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-03-05 10:43:06 -0500

    changes

--- cs101/in_class/march03.py
+++ cs101/in_class/march03.py
@@ -1,8 +1,8 @@
 def sum():
-    # sum starting from 0 up to variable number, 10 + 11 + 12 + 13 + .... + number - 1
-    #
-    #I am getting number - 1 + 10 at the end, while I wan to have number - 1                                0, 1, 2, 3, ... + number - 1
-    number = int(input('Enter an integer greater than 10: '))
+    # sum starting from 0 up to variable number, 10 + 11 + 12 + 13 + .... + number - 1 the length of this list is number - 10
+    #                                            0, 1, 2, 3, ... + number - 1
+    #I am getting number - 1 + 10 at the end, while I wan to have number - 1
+    number = int(input('Enter an integer: '))
     total = 0
     for i in range(number - 10): # start from 0 go up to n - 1, e.g. i = 0, 1, 2, 3, 4
         # in single argument range(), we only change the ending point. The start is always 0 by default.
