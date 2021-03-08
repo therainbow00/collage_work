1e1a1abd48250ea8b5c0215ce43fadcb43187e4c
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-03-08 09:50:05 -0500

    changes

--- cs101/in_class/march05.py
+++ cs101/in_class/march05.py
@@ -5,8 +5,8 @@ def sum():
     #print the list below
     #   -number, -number + 1, ..., 0
     # [0,1,2,3,...,n]
-    #list_number = [913, 123, 342, 23]
-    #print(list_number[2])
+    list_number = [913, 123, 342, 23]
+    print(list_number[2])
     #Ex: print a list of number such that if each number is even add 10, if its odd add 20:
 
     number = int(input('Enter an integer (positive): '))
@@ -14,7 +14,7 @@ def sum():
     #total = 0
     for i in range(number): # start from 0 go up to n - 1, e.g. i = 0, 1, 2, 3, 4
         #14, 12, 10, 8, 6, 4, 2, 0
-        #print(i)
+        print(i)
         # in single argument range(), we only change the ending point. The start is always 0 by default.
         #even
         i = i + 10  #setting the starting point
---
+++ cs101/in_class/march08.py
@@ -0,0 +1,35 @@
+'''
+starting from 2 to 14, if number is even, multiply by 2 and if not multiply by 3. At the end sum everything and return an integer
+'''
+def conditional_sum(end):
+    total = 0
+    #end = int(input('Enter your upper bound: '))
+    for i in range(2, end + 1):
+        if i % 2 == 0:
+            #print('even', i)
+            total += i * 2
+        else:
+            #print('odd', i)
+            total += i * 3
+    return total
+
+def conditional_sum2(end1):
+    total = 0
+    #end = int(input('Enter your upper bound: '))
+    i = 2
+    while i < end1 + 1:
+        if i % 2 == 0:
+            #print('even', i)
+            total += i * 2
+        else:
+            #print('odd', i)
+            total += i * 3
+        i += 1
+    return total
+
+upper_bound = int(input('Enter an upper bound: '))
+#conditional_sum()
+print(conditional_sum(upper_bound))
+print('---------------------------conditional_sum2-----')
+#conditional_sum2()
+print(conditional_sum2(upper_bound))
