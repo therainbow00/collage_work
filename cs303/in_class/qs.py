b6dace0975d8a90464a4e351bc1a55ac0d70c8da
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-04-01 10:54:03 -0400

    changes

--- cs303/in_class/qs.py
+++ cs303/in_class/qs.py
@@ -13,7 +13,7 @@ def partition(A, p, r):
     for j in range(p, r):
         if A[j] <= x:
             i += 1
-            A[i], A[j] = A[j], A[i]
+            A[i], A[j] = A[j], A[i]  #swap
     A[i + 1], A[r] = A[r], A[i + 1]
     return i + 1
 
