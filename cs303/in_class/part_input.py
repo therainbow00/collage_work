71385eefe38f16981c39de513d240c64e2bcf9d3
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-04-02 10:50:53 -0400

    changes

--- cs303/in_class/part_input.py
+++ cs303/in_class/part_input.py
@@ -1,10 +1,16 @@
 #A = [30, 20, 10, 50, 60, 3, 40]
-A = [30, 20, 10, 50, 60, 3, 40]
+#A = [30, 20, 10, 50, 60, 3, 40]
 '''
 use the right hand number as a pivot
 part1 numbers less than the pivot (40)
 part2 number greater than or equal to the pivot
 '''
+A = []
+num = int(input('Enter the number of elements in the array: '))
+for i in range(num):
+    element = int(input('Enter element: '))
+    A.append(element)
+#print(A)
 
 def partition(A, p, r):
     x = A[r]  # get the pivot
@@ -16,7 +22,11 @@ def partition(A, p, r):
             A[i], A[j] = A[j], A[i] #swap
     A[i + 1], A[r] = A[r], A[i + 1]
     return i + 1
-
+'''
 print(A)
 partition(A, 0, 6)
+print(A)
+'''
+print(A)
+partition(A, 0, num - 1)
 print(A)
