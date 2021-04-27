a37ad3e7da244fa1c775006f0c0aa7ba5c7817bc
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-04-27 10:54:04 -0400

    changes

--- cs303/in_class/conn.py
+++ cs303/in_class/conn.py
@@ -1,10 +1,10 @@
 adj = [
-        [0, 0, 0, 1, 1, 0, 0]
-        [0, 0, 1, 0, 0, 1, 0]
-        [0, 1, 0, 0, 0, 0, 1]
-        [1, 0, 0, 0, 1, 0, 0]
-        [1, 0, 0, 1, 0, 0, 0]
-        [0, 1, 0, 0, 0, 0, 1]
+        [0, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 1],
         [0, 0, 1, 0, 0, 1, 0]
       ]
 '''
@@ -25,8 +25,17 @@ breadth first search:
 from collections import deque
 queue = deque() #vertices left to be processed
 lst = [] #already processed
-queue.append(0)
+size = len(adj)
+
+start = int(input('Enter an integer 0 - '+ str(size - 1) +'> '))
+queue.append(start) #new vertices that need to be processed
+'''                 processing a vertex, V: finding all the vertices
+                                            that are connected to V by an edge
+                                            an edge are not already in the queue
+                                            or have not already been processed'''
+lst.append(start)
 while queue:
+    #get the vertex to be processed
     x = queue.popleft()
     #process the vertex just removed
     for i in range(len(adj)):
