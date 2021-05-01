3ef4aacbe2d7b00beefb94e5d4098da22a73bf43
therainbow00 <60056454+therainbow00@users.noreply.github.com>
2021-04-30 21:48:53 -0400

    changes

--- cs101/labs/final/ttt.py
+++ cs101/labs/final/ttt.py
@@ -17,23 +17,50 @@ def print_board(board):
 def user_move(board, player):
     move = int(input('Enter your move (1 - 9): '))
     if move == 1:
-        board[0][0] = player
+        if board[0][0] == ' ':
+            board[0][0] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 2:
-        board[0][1] = player
+        if board[0][1] == ' ':
+            board[0][1] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 3:
-        board[0][2] = player
+        if board[0][2] == ' ':
+            board[0][2] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 4:
-        board[1][0] = player
+        if board[1][0] == ' ':
+            board[1][0] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 5:
-        board[1][1] = player
+        if board[1][1] == ' ':
+            board[1][1] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 6:
-        board[1][2] = player
+        if board[1][2] == ' ':
+            board[1][2] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 7:
-        board[2][0] = player
+        if board[2][0] == ' ':
+            board[2][0] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 8:
-        board[2][1] = player
+        if board[2][1] == ' ':
+            board[2][1] = player
+        else:
+            print(f'"{move}" already taken.')
     elif move == 9:
-        board[2][2] = player
+        if board[2][2] == ' ':
+            board[2][2] = player
+        else:
+            print(f'"{move}" already taken.')
 
 def check_for_winner(board, player):
     #check rows
