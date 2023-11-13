9dd5fd0b20de4728b72f9ce08f8a1493a04e9692
the_rainbow_00 <60056454+therainbow00@users.noreply.github.com>
2023-11-12 19:55:08 -0600

    Delete cs303 directory

--- cs303/in_class/fair.py
+++
@@ -1,8 +0,0 @@
-import random
-
-lst = [0, 0, 0, 0, 0, 0, 0]
-for i in range(60000):
-    outcome = random.randint(1, 6)
-    lst[outcome] += 1
-
-print(lst)
--- cs303/in_class/p4.py
+++
@@ -1,7 +0,0 @@
-n = 10
-x = 0
-for i in range(n):
-    for j in range(n):
-        for k in range(3):
-            x += 1
-print(x)
