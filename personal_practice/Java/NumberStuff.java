b780954bc019e5c24201850206c2bbd9cadb1783
the_rainbow_00 <60056454+therainbow00@users.noreply.github.com>
2023-12-09 12:15:49 -0600

    changes

--- personal_practice/Java/NumberStuff.java
+++ personal_practice/Java/NumberStuff.java
@@ -2,7 +2,7 @@ package personal_practice.Java;
 
 import java.util.Random;
 
-public class test 
+public class NumberStuff
 {
     public static void main(String[] args) 
     {
@@ -12,18 +12,12 @@ public class test
         int num = Integer.parseInt(args[0]);
         Total total;
 
-        for (int i = 0; i < num; i++) 
+        for (int i = 1; i <= num; i++) 
         {
             total = new Total(rand.nextInt(101));
             SetNumbersInt(rand, numFunctions);
             SetNumbersDecimal(rand, numFunctions);
-            System.out.printf("The greater number is: %d (Number 1 was: %d | Number 2 was: %d)\n", numFunctions.Greater(numFunctions.GetNum1(), numFunctions.GetNum2()), numFunctions.GetNum1(), numFunctions.GetNum2());
-            System.out.println("Total: " + total.GetTotal());
-            System.out.printf("The greater number is: %.0f (Number 3 was: %.0f | Number 4 was: %.0f)\n", numFunctions.Greater(numFunctions.GetNum3(), numFunctions.GetNum4()), numFunctions.GetNum3(), numFunctions.GetNum4());
-            total = new Total(rand.nextInt(101));
-            System.out.println("Total: " + total.GetTotal());
-            for (int l = 1; l <= 70; l++) {System.out.print("=");}
-            System.out.println();
+            PrintStuff(numFunctions, total, rand, i);
         }
         System.out.println(nums.Print());
     }
@@ -45,9 +39,33 @@ public class test
         length = rand.nextInt(100);
         numFunctions.SetNum2Or4(length);
     }
+
+    static void PrintStuff(NumFunctions numFunctions, Total total, Random rand, int index)
+    {
+        for (int l = 1; l <= 33; l++) {System.out.print("=");}
+        System.out.printf(" %d ", index);
+
+        if (index > 9) for (int l = 1; l <= 33; l++) {System.out.print("=");}
+        else for (int l = 1; l <= 34; l++) {System.out.print("=");}
+
+        System.out.println();
+
+        System.out.printf("The greater number is: %d (Number 1 was: %d | Number 2 was: %d)\n", numFunctions.Greater(numFunctions.GetNum1(), numFunctions.GetNum2()), numFunctions.GetNum1(), numFunctions.GetNum2());
+
+        System.out.println("Total: " + total.GetTotal());
+        System.out.printf("The greater number is: %.0f (Number 3 was: %.0f | Number 4 was: %.0f)\n", numFunctions.Greater(numFunctions.GetNum3(), numFunctions.GetNum4()), numFunctions.GetNum3(), numFunctions.GetNum4());
+
+        total = new Total(rand.nextInt(101));
+        System.out.println("Total: " + total.GetTotal());
+
+        for (int l = 1; l <= 70; l++) {System.out.print("=");}
+
+        System.out.println();
+    }
 }
 
-class Nums extends Done {
+class Nums extends Done
+{
     protected int num1;
     protected int num2;
 
@@ -76,25 +94,21 @@ class Total extends Nums
 
 class NumFunctions extends Nums 
 {
-    protected int GetNum1() {return num1;}
-
     protected void SetNum1Or3(int num) {num1 = num;}
-
     protected void SetNum1Or3(double num) {num3 = num;}
 
     protected int GetNum2() {return num2;}
+    protected int GetNum1() {return num1;}
 
     protected double GetNum3() {return num3;}
-
     protected double GetNum4() {return num4;}
 
     protected void SetNum2Or4(int num) {num2 = num;}
-
     protected void SetNum2Or4(double num) {num4 = num;}
 
     protected int Greater(int num1, int num2) {if (num1 < num2) {return num2;} return num1;}
-
     protected double Greater(double num1, double num2) {if (num1 < num2) {return num2;} return num1;}
 }
+
 
 abstract class Done {abstract String Print();}
