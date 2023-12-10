4a749a068a5166ba081b3ef0cfe244c737217521
the_rainbow_00 <60056454+therainbow00@users.noreply.github.com>
2023-12-09 20:53:57 -0600

    changes

--- personal_practice/Numbers/Numbers/Program.cs
+++ personal_practice/Numbers/Numbers/Program.cs
@@ -5,14 +5,16 @@ using static System.Console;
 
 namespace Numbers
 {
-    public class NumberStuff
+    internal class NumberStuff
     {
-        public static void main(string[] args)
+        internal static void Main(string[] args)
         {
             Random rand = new Random();
             Nums nums = new Nums();
             NumFunctions numFunctions = new NumFunctions();
-            int num = int.Parse(args[0]);
+            Write("Number of loops: ");
+            string? input = ReadLine();
+            int num = int.Parse(input);
             Total total;
 
             for (int i = 1; i <= num; i++)
@@ -38,36 +40,36 @@ namespace Numbers
         {
             double length;
             length = rand.Next(100);
-            numFunctions.SetNum1Or3(length);
+            numFunctions.SetNum1Or3(length + .75);
             length = rand.Next(100);
-            numFunctions.SetNum2Or4(length);
+            numFunctions.SetNum2Or4(length + .56);
         }
 
         static void PrintStuff(NumFunctions numFunctions, Total total, Random rand, int index)
         {
-            for (int l = 1; l <= 33; l++) { System.out.print("="); }
-            System.out.printf(" %d ", index);
+            for (int l = 1; l <= 33; l++) Write("=");
+            Write($" {index} ");
 
-            if (index > 9) for (int l = 1; l <= 33; l++) { System.out.print("="); }
-            else for (int l = 1; l <= 34; l++) { System.out.print("="); }
+            if (index > 9) for (int l = 1; l <= 33; l++) Write("=");
+            else for (int l = 1; l <= 34; l++) Write("=");
 
-            System.out.println();
+            WriteLine();
 
-            System.out.printf("The greater number is: %d (Number 1 was: %d | Number 2 was: %d)\n", numFunctions.Greater(numFunctions.GetNum1(), numFunctions.GetNum2()), numFunctions.GetNum1(), numFunctions.GetNum2());
+            WriteLine($"The greater number is: {numFunctions.Greater(numFunctions.Num1, numFunctions.Num2)} (Number 1 was: {numFunctions.Num1} | Number 2 was: {numFunctions.Num2})");
 
-            System.out.println("Total: " + total.GetTotal());
-            System.out.printf("The greater number is: %.0f (Number 3 was: %.0f | Number 4 was: %.0f)\n", numFunctions.Greater(numFunctions.GetNum3(), numFunctions.GetNum4()), numFunctions.GetNum3(), numFunctions.GetNum4());
+            WriteLine("Total: " + total.TotalNumber);
+            WriteLine($"The greater number is: {numFunctions.Greater(numFunctions.Num3, numFunctions.Num4).ToString("0")} (Number 3 was: {numFunctions.Num3.ToString("0")} | Number 4 was: {numFunctions.Num4.ToString("0")})");
 
-            total = new Total(rand.nextInt(101));
-            System.out.println("Total: " + total.GetTotal());
+            total = new Total(rand.Next(101));
+            WriteLine("Total: " + total.TotalNumber);
 
-            for (int l = 1; l <= 70; l++) { System.out.print("="); }
+            for (int l = 1; l <= 70; l++) Write("=");
 
-            System.out.println();
+            WriteLine();
         }
     }
 
-    public class Nums : Done
+    internal class Nums : Done
     {
         protected int num1;
         protected int num2;
@@ -78,16 +80,19 @@ namespace Numbers
         public override string Print() { return "Done"; }
     }
 
-    public class Total : Nums
+    internal class Total : Nums
     {
         private int totalNumber;
 
-        public Total(int num) { SetTotal(PrintNums(num)); }
+        public Total(int num) { TotalNumber = (PrintNums(num)); }
 
-        protected int TotalNumber { get; set; }
+        public int TotalNumber
+        {
+            get { return totalNumber; }
+            set { totalNumber = value; }
+        }
 
-        protected void SetTotal(int num) { TotalNumber = num; }
-        protected int PrintNums(int length)
+        private int PrintNums(int length)
         {
             int temp = 0;
             for (int i = 0; i < length; i++) temp += i;
@@ -95,12 +100,28 @@ namespace Numbers
         }
     }
 
-    public class NumFunctions : Nums
+    internal class NumFunctions : Nums
     {
-        protected int Num1 { get; set; }
-        protected int Num2 { get; set; }
-        protected double Num3 { get; set; }
-        protected double Num4 { get; set; }
+        public int Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+        public int Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+        public double Num3
+        {
+            get { return num3; }
+            set { num3 = value; }
+        }
+        public double Num4
+        {
+            get { return num4; }
+            set { num4 = value; }
+        }
 
         public void SetNum1Or3(int num) { Num1 = num; }
         public void SetNum1Or3(double num) { Num3 = num; }
@@ -108,10 +129,10 @@ namespace Numbers
         public void SetNum2Or4(int num) { Num2 = num; }
         public void SetNum2Or4(double num) { Num4 = num; }
 
-        protected int Greater(int num1, int num2) { if (num1 < num2) return num2; return num1; }
-        protected double Greater(double num1, double num2) { if (num1 < num2) return num2; return num1; }
+        public int Greater(int num1, int num2) { if (num1 < num2) return num2; return num1; }
+        public double Greater(double num1, double num2) { if (num1 < num2) return num2; return num1; }
     }
 
 
-    abstract class Done { protected abstract string Print(); }
+    internal abstract class Done { public abstract string Print(); }
 }
